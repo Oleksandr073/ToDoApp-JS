5cd3f27cd09122c70b659317b3b64ec8652f84d2
Oleksandr <oleksandr073403@gmail.com>
2022-11-24 22:53:45 +0200

    adds constant dataAttributes

---
+++ client/src/js/constants/dataAttributes.js
@@ -0,0 +1,4 @@
+export const darkThemeAttribute = 'data-dark-theme';
+export const menuOpenAttribute = 'data-menu-open';
+export const modalOpenAttribute = 'data-modal-open';
+export const taskIdAttribute = 'data-task-id';
